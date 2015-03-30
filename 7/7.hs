0a83cb4abbd359871d799e48a564ec90885f3fcb
Sam Liao <phyomh@gmail.com>
2015-03-30 19:19:19 +0800

    Update 7.hs

--- 7/7.hs
+++ 7/7.hs
@@ -13,8 +13,8 @@ data JoinList m a = Empty
 -- append function
 
 tag :: Monoid m => JoinList m a -> m
-tag Empty            = mempty
-tag (Single m _)     = m  
+tag Empty          = mempty
+tag (Single m _)   = m  
 tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
@@ -22,9 +22,42 @@ tag (Append m _ _) = m
 
 -- exercise 2
 
-indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a 
-indexJ n jl 
-    | n < 0 = Nothing
-    | n > (getSize . tag jl) = Nothing
-    | _ Empty = Nothing
+-- get the nth element of a list
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ _ Empty          = Nothing
+indexJ _ (Single _ x)   = Just x
+indexJ n (Append m j1 j2)
+    | n < 0                     = Nothing
+    | n >= (getSize . size $ m) = Nothing
+    | n <  (getSize . size . tag $ j1)  = indexJ n j1
+    | otherwise                         = indexJ (n - (getSize . size . tag $ j1)) j2
 
+-- convert a JoinList to a list
+jlToList :: JoinList m a -> [a]
+jlToList Empty              = []
+jlToList (Single _ a)       = [a]
+jlToList (Append _ l1 l2)   = jlToList l1 ++ jlToList l2
+
+-- drops the first n elements from a JoinList
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty           = Empty
+dropJ 1 (Single _ _)    = Empty
+dropJ _ jl@(Single _ _) = jl
+dropJ n jl@(Append m j1 j2)
+    | n < 0                             = jl
+    | n >= (getSize . size $ m)         = Empty
+    | n <  (getSize . size . tag $ j1)  = (dropJ n j1) +++ j2
+    | otherwise                         = dropJ (n - (getSize . size . tag $ j1)) j2
+
+-- take the first n elements of the a JoinList
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty           = Empty
+takeJ 0 _               = Empty
+takeJ _ jl@(Single _ _) = jl
+takeJ n jl@(Append m j1 j2)
+    | n < 0                             = Empty
+    | n >= (getSize . size $ m)         = jl
+    | n <  (getSize . size . tag $ j1)  = takeJ n j1
+    | otherwise                         = j1 +++ (takeJ (n - (getSize . size . tag $ j1)) j2)
+
+-- exercise 3
