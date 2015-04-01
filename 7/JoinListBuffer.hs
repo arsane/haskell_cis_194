1df794018865db5074e56bfd51ba74f607d6d722
Sam Liao <phyomh@gmail.com>
2015-04-01 18:34:28 +0800

    modify the fromString algorithm for better performance

--- 7/JoinListBuffer.hs
+++ 7/JoinListBuffer.hs
@@ -9,9 +9,9 @@ import Scrabble
 import Buffer
 
 data JoinList m a = Empty
-                |   Single m a
-                |   Append m (JoinList m a) (JoinList m a)
-    deriving (Eq, Show)
+  |   Single m a
+  |   Append m (JoinList m a) (JoinList m a)
+  deriving (Eq, Show)
 
 -- exercise 1
 
@@ -19,7 +19,7 @@ data JoinList m a = Empty
 
 tag :: Monoid m => JoinList m a -> m
 tag Empty          = mempty
-tag (Single m _)   = m  
+tag (Single m _)   = m
 tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
@@ -32,10 +32,10 @@ indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty          = Nothing
 indexJ _ (Single _ x)   = Just x
 indexJ n (Append m j1 j2)
-    | n < 0                     = Nothing
-    | n >= (getSize . size $ m) = Nothing
-    | n <  (getSize . size . tag $ j1)  = indexJ n j1
-    | otherwise                         = indexJ (n - (getSize . size . tag $ j1)) j2
+  | n < 0                     = Nothing
+  | n >= (getSize . size $ m) = Nothing
+  | n <  (getSize . size . tag $ j1)  = indexJ n j1
+  | otherwise                         = indexJ (n - (getSize . size . tag $ j1)) j2
 
 -- convert a JoinList to a list
 jtToList :: JoinList m a -> [a]
@@ -49,10 +49,10 @@ dropJ _ Empty           = Empty
 dropJ 1 (Single _ _)    = Empty
 dropJ _ jt@(Single _ _) = jt
 dropJ n jt@(Append m j1 j2)
-    | n < 0                             = jt
-    | n >= (getSize . size $ m)         = Empty
-    | n <  (getSize . size . tag $ j1)  = (dropJ n j1) +++ j2
-    | otherwise                         = dropJ (n - (getSize . size . tag $ j1)) j2
+  | n < 0                             = jt
+  | n >= (getSize . size $ m)         = Empty
+  | n <  (getSize . size . tag $ j1)  = (dropJ n j1) +++ j2
+  | otherwise                         = dropJ (n - (getSize . size . tag $ j1)) j2
 
 -- take the first n elements of the a JoinList
 takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
@@ -60,10 +60,10 @@ takeJ _ Empty           = Empty
 takeJ 0 _               = Empty
 takeJ _ jt@(Single _ _) = jt
 takeJ n jt@(Append m j1 j2)
-    | n < 0                             = Empty
-    | n >= (getSize . size $ m)         = jt
-    | n <  (getSize . size . tag $ j1)  = takeJ n j1
-    | otherwise                         = j1 +++ (takeJ (n - (getSize . size . tag $ j1)) j2)
+  | n < 0                             = Empty
+  | n >= (getSize . size $ m)         = jt
+  | n <  (getSize . size . tag $ j1)  = takeJ n j1
+  | otherwise                         = j1 +++ (takeJ (n - (getSize . size . tag $ j1)) j2)
 
 -- exercise 3
 -- scoreLine "yay " +++ scoreLine "haskell!"
@@ -71,10 +71,17 @@ scoreLine :: String -> JoinList Score String
 scoreLine s = Single (scoreString s) s
 
 -- exercise 4
+-- add element at the list head
+addJ :: (Sized b, Monoid b) => (a -> b) -> a -> JoinList b a -> JoinList b a
+addJ f x Empty              = (Single (f x) x)
+addJ f x jt@(Single _ _)    = (Single (f x) x) +++ jt
+addJ f x jt@(Append _ j1 j2)
+    | (getSize . size . tag $ j1) == (getSize . size . tag $ j2) = (Single (f x) x) +++ jt
+    | otherwise             = (addJ f x j1) +++ j2
 
 instance Buffer (JoinList (Score, Size) String) where
   toString           = init . unlines . jtToList
-  fromString      s  = foldr (\x acc -> x +++ acc) Empty (map (\x -> Single (scoreString x, Size 1) x) (lines s))
+  fromString      s  = foldr (addJ (\x -> (scoreString x, Size 1))) Empty (lines s)
   line               = indexJ
   replaceLine n l b  = takeJ n b +++ fromString l +++ dropJ (n + 1) b
   numLines           = getSize  . snd . tag
--- 7/Test.hs
+++ 7/Test.hs
@@ -9,3 +9,8 @@ import Sized
 main = do
     f   <- readFile "carol.txt"
     return ((fromString f) :: JoinList (Score, Size) String)
+
+carolJoinList :: IO (JoinList (Score, Size) String)
+carolJoinList = do
+    f  <- readFile "carol.txt"
+    return ((fromString f) :: JoinList (Score, Size) String)
