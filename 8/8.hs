efacdd78f77540c399b46465cf4f64b3ce9aeefe
Sam Liao <phyomh@gmail.com>
2015-04-05 14:26:46 +0800

    add main to 8.hs

--- 8/8.hs
+++ 8/8.hs
@@ -3,6 +3,7 @@
 
 module GuestList where
 
+import Data.List
 import Data.Monoid
 import Data.Tree
 import Employee
@@ -16,21 +17,23 @@ instance Monoid GuestList where
 glCons :: Employee -> GuestList -> GuestList
 glCons e gl = (GL [e] (empFun e)) <> gl
 
-moreFun :: GuestList -> GuestList -> GuestList
-moreFun x@(GL _ f1) y@(GL _ f2) | f1 > f2   = x
+-- dump way
+moreFun1 :: GuestList -> GuestList -> GuestList
+moreFun1 x@(GL _ f1) y@(GL _ f2) | f1 > f2   = x
                                 | otherwise = y
+-- Ord GuestList
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun = max
 
 -- exercise 2 
 
 treeFold :: (a -> [b] -> b) -> Tree a -> b
-treeFold f (Node x []) = f x []
 treeFold f (Node x xs) = f x (map (treeFold f) xs)
 
 -- exercise 3
 -- instance (Monoid a, Monoid b) => Monoid (a, b)
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel e [] = (GL [e] (empFun e), mempty)
 nextLevel e gs  = nextgl e $ foldr (\x y -> x <> y) mempty gs
 					where nextgl em (sx, sy) = (glCons em sy, sx)
 
@@ -44,12 +47,18 @@ maxFun = (uncurry moreFun) . treeFold nextLevel
 
 -- exercise 5
 
+getNames :: GuestList -> ([Name], Fun)
+getNames (GL xs f) = (sort $ map empName xs, f)
+
 -- read company's hierarchy from file company.txt and then prints out
 -- a formatted guest list, sorted by first name.
 -- Total fun: xxxx
 -- Adam Debergues
 -- xxx xxxx 
 
--- main :: IO ()
-
-
+main :: IO ()
+main = do
+	f <- readFile "company.txt"
+	let guestlist = getNames (maxFun (read f))
+	putStrLn ("Total fun: " ++ show (snd guestlist))
+	mapM_ putStrLn (fst guestlist)
