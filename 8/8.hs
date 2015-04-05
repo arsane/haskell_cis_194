faac58ca854380eec041dc2483111d696c8ecf26
Sam Liao <phyomh@gmail.com>
2015-04-05 12:56:05 +0800

    add maxFun

--- 8/8.hs
+++ 8/8.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module GuestList where
 
@@ -16,7 +17,7 @@ glCons :: Employee -> GuestList -> GuestList
 glCons e gl = (GL [e] (empFun e)) <> gl
 
 moreFun :: GuestList -> GuestList -> GuestList
-moreFun x@(GL _ f1) y@(GL _ f2) | f1 < f2   = x
+moreFun x@(GL _ f1) y@(GL _ f2) | f1 > f2   = x
                                 | otherwise = y
 
 -- exercise 2 
@@ -26,13 +27,20 @@ treeFold f (Node x []) = f x []
 treeFold f (Node x xs) = f x (map (treeFold f) xs)
 
 -- exercise 3
+-- instance (Monoid a, Monoid b) => Monoid (a, b)
 
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e [] = (GL [e] (empFun e), mempty)
+nextLevel e gs  = nextgl e $ foldr (\x y -> x <> y) mempty gs
+					where nextgl em (sx, sy) = (glCons em sy, sx)
 
 -- exercise 4
 
--- maxFun :: Tree Employee -> GuestList
+-- maxFun testCompany
+-- maxFun testCompany2
 
+maxFun :: Tree Employee -> GuestList
+maxFun = (uncurry moreFun) . treeFold nextLevel
 
 -- exercise 5
 
