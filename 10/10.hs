c9ca74f74521367dbfe2054d939d6f8527f05f08
Sam Liao <phyomh@gmail.com>
2015-04-25 15:49:09 +0800

    improve Parser's Applicative impl

--- 10/10.hs
+++ 10/10.hs
@@ -22,15 +22,50 @@ instance Functor Parser where
 		where h xs = fmap (first f) (g xs)
 
 -- exercise 2
--- to be improved
+{-
+type Name = String
+data Employee = Emp { name :: Name, phone :: String }
+
+parseName  :: Parser Name
+parsePhone :: Parser String
+
+then
+
+Emp <$> parseName <*> parsePhone :: Parser Employee
+
+--
+-- Maybe Applicative
+-- only param 1 and param 2 both Just, then result is Just.
+-- otherwise Nothing.
+(<*>) :: Maybe (a->b) ->  Maybe a -> Maybe b
+
+-- Parser Applicative
+(<*>) :: (String -> Maybe (a->b, String)) ->
+	     (String -> Maybe (a, String))    ->
+	     (String -> Maybe (b, String))
+
+Maybe (a->b) -> Maybe a -> Maybe b
+--
+-- Monad
+Maybe String -> (String -> Maybe (a, String)) -> Maybe (a, String) -- >>=
+(a->b, String) -> Maybe (a, String) -> Maybe (b, String)
+Maybe (a->b, String) -> Maybe (a, String) -> Maybe (b, String)
+Maybe (a->b) -> Maybe a -> Maybe b
+Maybe b -> Maybe String -> Maybe (b, String)
+b -> String -> (b, String)
+--
+(a -> b) -> f a -> f b
+(fmap snd)   :: Functor f => f (a, b) -> f b
+(fmap first) :: Functor f => f (a, b) -> f a
+-}
+
 instance Applicative Parser where
-	pure  a = Parser h where h str = Just (a, str)
+	pure a = Parser h where h str = Just (a, str)
+	-- use Maybe Applicative
 	(Parser f) <*> (Parser g) = Parser h
-		where h xs = case f xs of
-			Nothing 	  -> Nothing
-			Just (y, str) -> case g str of
-				Nothing   		-> Nothing
-				Just (z, str') 	-> Just (y z, str')
+		where h xs = (\a b -> (a, b)) <$> ((fst <$> t) <*> (fst <$> u)) <*> (snd <$> u) where
+				t = f xs
+				u = (snd <$> t)	>>= g
 
 -- exercise 3
 
